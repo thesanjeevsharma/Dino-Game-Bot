7d7ce4fb4b197d028d35633426598b82c905775a
Sanjeev Sharma <godoflovenwar@gmail.com>
2018-08-12 17:32:30 +0530

    working

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
-#from PIL import ImageGrab
+from PIL import ImageGrab, ImageOps
 import pyautogui
 import time
+from numpy import *
 
 class Cordinates():
-    replayBtn = (340, 490)
-    dino = (260, 490)
-
+    #These cordiantes will change as per your screen.
+    replayBtn = (340, 310)
+    dino = (90, 315)
+    #330
+    #90
+    
 def restartGame():
     pyautogui.click(Cordinates.replayBtn)
 
@@ -15,7 +19,20 @@ def jump():
     print('Jump')
     pyautogui.keyUp('space')
 
+def imageGrab():
+    box = (Cordinates.dino[0]+(150),Cordinates.dino[1],Cordinates.dino[0]+(150)+40,Cordinates.dino[1]+30)
+    image = ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    a = array(grayImage.getcolors())
+    print(a.sum())
+    return a.sum()
 
-restartGame()
-time.sleep(1)
-jump()
+def main():
+    restartGame()
+    while True:
+        if(imageGrab()!= 1538):
+            jump()
+            time.sleep(0.1)
+
+
+main()
